113086651edaae91b3c60cb0ca86d1a09ec6e73d
Filip Lav Maksimovic <gaming4ever93@gmail.com>
2017-10-13 03:19:50 +0200

    Added the algorithm
    Added the algorithm #forgive me if I didn't understand the task well.
    Removed import of re and time from Search, also Removed spider.py as it's unnecessary.

--- algorithm.py
+++ algorithm.py
@@ -1,19 +1,18 @@
 import requests
 import json
-import bs4 as bs
 
-#http://www.urbanhome.ch/Search/DoSearch
 position = 0
 skip = 0
 count = 0
 numreq = 1
-lista=[]
-f = open('jsondata.json','a')
+poslist = []
+skiplist = []
 while(1):
 
     position += count
     skip += count
-
+    poslist.append(position)
+    skiplist.append(skip)
     payload = {
       "settings": {
         "MainTypeGroup": "1",
@@ -64,59 +63,19 @@ while(1):
     cont = json.loads(r.content)
     print(cont.get('MaximumReached'))
     if(str(cont.get('MaximumReached'))=='True'):
-        print("Pages it took: ", numreq)
+        print("Ima ", numreq, "stranice")
+        print("POST parametri:\n" + "position: ", (poslist),"\nskiplist:", skiplist)
         break
     numreq += 1
     #print(cont)
-    rows = (cont.get('Rows'))
     #print(rows)
     count = (cont.get('Count'))
     print(count)
-    #print(type(rows))
-    #print(rows)
-    rows=rows.encode('latin1').decode('unicode-escape')
-    #print(rows)
-    soup = bs.BeautifulSoup(rows,'lxml')
-    #print(soup)
-    i=0
-
-    for ad in soup.find_all('div', class_='a ax'):
-        i+=1
-        url = (ad.find('a').get('href'))
-        title =  (ad.find('a').text)
-        adress = (ad.find('div', class_='a ay'))
-        street = (adress.find('p',class_='pt15').text)
-        pcode = (adress.find('p',class_='').text)
-        equipment = []
-        for equi in ad.find_all('span', class_='fl pr6'):
-            equipment.append(equi.text)
-        print(street)
-        print(pcode)
-        print("TITLE: " + title + "\nURL: " + url + "\n Adress: " + street + " " + pcode + "\nEquipment: " + str(equipment))
-        equipment = [item[1:len(item)] for item in equipment]
-        print(equipment)
 
 
 
-        jsondict = {
-            "name" : title,
-            "address" : {
-                "street": street,
-                "city": "Winterthur",
-        		"zipcode":8400
-                },
-            "additionalFeatures" : {
-                "equipment" : {item : 'true' for item in equipment }
-            }
 
-        }
-        print(jsondict)
 
-        lista.append(jsondict)
-        print(i)
-jsonlista = json.dumps(lista)
-f.write(jsonlista)
-f.close()
 
 
     #print(unicoded)
